44f79547fed1064ea20e12ebb22fcad5611ff5e0
christopher <christophergrant@openmailbox.org>
2016-11-09 23:09:36 -0800

    init commit, user input

---
+++ main.c
@@ -0,0 +1,31 @@
+/* 
+
+    This program generates two random integer arrays of user given
+    dimensions, and multiplies them using parallel processing, displaying
+    the result.
+
+
+
+    Programmers: Christopher Grant and  
+    Date: November 15, 2016
+*/
+
+
+#include "stdio.h"
+#include "stdlib.h"
+
+int main()
+{
+    int n;
+    /* ask user for input, how large will these matrices be?
+     allow for only one input of nxn, same dimension matrices
+     this is only a weeklong project, maybe come back later? */
+    printf("Hello, how large would you like the matrices to be? (nxn) ");
+    scanf("%d", &n);
+
+    // generate a random integer array with given dimension
+    // malloc will have to be used, using an array 
+        
+
+    // multiply the arrays
+}
