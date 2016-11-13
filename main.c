602992ff6bb86190f839969d0c5853b68d61b00d
christopher <christophergrant@openmailbox.org>
2016-11-13 00:29:14 -0800

    major changes, finished the assignment, started report

--- main.c
+++ main.c
@@ -8,65 +8,189 @@
     Date: November 15, 2016
 */
 
-#include "stdio.h"
-#include "stdlib.h"
-#include "time.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
 
-void printArray(int ** array, int n)
-{
-    printf("Matrix of size %dx%d: \n", n,n);
-    for(int i = 0; i < n; i++)
-    {
-        for(int j = 0; j < n; j++)
-        {
-            printf("%d ", array[i][j]);
-        }
-        printf("\n");
-    }
-}
+// prototypes
+void *prod_worker(void *arg);
+void product(int id, int *rows, int dim, double **matrix_a,
+                double **matrix_b, double **matrix_r);
+void print_matrix(double **matrix, int dim);
+
+/* 
+    This struct will be passed along with each thread
+
+    id = the thread #
+    dim = nxn dimension of the matrix
+    m1 = matrix A
+    m2 = matrix B
+    mr = resultant matrix
+    rows = used to determine which thread handles which row
+*/
+typedef struct {
+    double **m1;
+    double **m2;
+    double **mr;
+    int id;
+    int dim;
+    int *rows;
+} s_param;
 
 
 int main()
 {
-    int n;
-    time_t t;
-    /* ask user for input, how large will these matrices be?
-     allow for only one input of nxn, same dimension matrices
-     this is only a weeklong project, maybe come back later? */
-    printf("Hello, how large would you like the matrices to be? (nxn) ");
-    scanf("%d", &n);
+    int dim_var, threads_var, rem_thread_var, rem_dim_var;
+    int thread_array[threads_var];
+    double **matrix_a, **matrix_b, **matrix_r;
+    pthread_t *thread;
+    pthread_attr_t attr_var;
+    long ret_val;
+    void *status;
+    // ask user for matrix dimension and num threads
+    printf("Hello, what size matrices would you like? (nxn) ");
+    scanf("%d", &dim_var);
+    printf("How many threads should this program use? ");
+    scanf("%d", &threads_var);
+       
+    srand48(time(NULL)); // seed rand with time
 
-    // generate 2 random integer arrays with given dimension
-    srand((unsigned) time(&t)); // seed rand with time
-    int **arrayOne = malloc(n * sizeof(int*));    
-    int **arrayTwo = malloc(n * sizeof(int*));
-    int **arrayResult = malloc(n * sizeof(int));
+    // alloc the first dim
+    matrix_a = malloc(dim_var  * sizeof(double *));
+    matrix_b = malloc(dim_var  * sizeof(double *));
+    matrix_r = malloc(dim_var  * sizeof(double *));
 
-    for(int i = 0; i < n; i++)
-    {
-        arrayOne[i] = malloc(n * sizeof(int));
-        arrayTwo[i] = malloc(n * sizeof(int));
-        arrayResult[i] = malloc(n * sizeof(int));
+    // alloc the second dim
+    for(int row = 0; row < dim_var; row++)
+    {        
+        matrix_a[row] = malloc(dim_var  * sizeof(double *));
+        matrix_b[row] = malloc(dim_var  * sizeof(double *));
+        matrix_r[row] = malloc(dim_var  * sizeof(double *));
     }
-
-    for(int i = 0; i < n; i++)
+    
+    // fill matrices with rand doubles
+    for(int row = 0; row < dim_var; row++)
     {
-        for(int j = 0; j < n; j++)
+        for(int col = 0; col < dim_var; col++)
         {
-            // values are under 10 for pretty-print, perhaps use fields
-            // to allow for pretty larger numbered matrices (up to 100?)
-            arrayOne[i][j] = rand() % 10;
-            arrayTwo[i][j] = rand() % 10;
+            matrix_a[row][col] = drand48(); 
+            matrix_b[row][col] = drand48();
+            matrix_r[row][col] = 0;
         }
     }
 
-    printArray(arrayOne, n);
-   
-    // multiply the arrays
+    // thread allocation and attribute initialization
+    thread = (pthread_t *) malloc(threads_var * sizeof(pthread_t));
+    pthread_attr_init(&attr_var);
+    pthread_attr_setdetachstate(&attr_var, PTHREAD_CREATE_JOINABLE);
 
     
-    // housekeeping and boilerplate
-    free(arrayOne);
-    free(arrayTwo);
+    // use the two rem(aining) variables to determine how many rows each thread
+    // will be handling. store the values in thread_array
+    rem_thread_var = threads_var;
+    rem_dim_var = dim_var;
+
+    for(int i = 0; i < threads_var; i++)
+    {
+        thread_array[i] = rem_dim_var / rem_thread_var;
+        rem_dim_var -= thread_array[i];
+        --rem_thread_var;
+    }
+    
+    // init our structure
+    s_param *arg;
+    arg = (s_param *) malloc(threads_var * sizeof(s_param));
+    
+    // loop through the number of threads setting each thread
+    // with this to correctly calculate its slice of the matrix
+    // this goes around the problem of pthreads, where you need
+    // static variables. we can use a struct as our static variable
+    for(int i = 0; i < threads_var; i++)
+    {
+        arg[i].id = i;
+        arg[i].rows = thread_array;
+        arg[i].dim = dim_var;
+        arg[i].m1 = &matrix_a;
+        arg[i].m2 = &matrix_b;
+        arg[i].mr = &matrix_r;
+
+        // spawn new thread
+        if(ret_val = pthread_create(&thread[i], &attr_var, prod_worker, (void *)(arg + i)))
+        {
+            fprintf(stderr, "error value %ld", ret_val);
+            exit(1);
+        }
+        printf("Thread %d started. \n", i+1);
+    }
+
+    // loop through each thread, joining it back to main
+    for(int i = 0; i < threads_var; i++)
+    {
+        ret_val = pthread_join(thread[i],&status);
+        if(ret_val)
+        {
+            fprintf(stderr, "error value %ld", ret_val);
+            exit(1);
+        }
+        printf("Thread %d completed. \n", i+1);
+    }
+    
+    // print results
+    printf("Matrix A: \n");
+    print_matrix(matrix_a, dim_var);
+    printf("Matrix B: \n");
+    print_matrix(matrix_b, dim_var);
+    printf("Resultant Matrix AB: \n");
+    print_matrix(matrix_r, dim_var);
     return 0;
+}
+
+
+
+void *prod_worker(void *arg)
+{
+    s_param *s = (s_param *)arg;
+    product(s->id, (s->rows), s->dim, *(s->m1), *(s->m2), *(s->mr));
+    pthread_exit(0);
+}
+
+
+void product(int id, int *rows, int dim, double **matrix_a,
+                double **matrix_b, double **matrix_r)
+{
+    int start_row, finish_row;
+    double sum;
+
+    for(int i = 0; i < id; i++)
+    {
+        start_row += rows[i];
+    }
+
+    finish_row = start_row + rows[id];
+    
+    for(int row = start_row; row < finish_row; row++)
+    {
+        for(int col = 0; col < dim; col++)
+        {
+            for(int k = 0; k < dim; k++)
+            {
+                sum = sum + (matrix_a[row][k] * matrix_b[k][col]);
+            }
+            matrix_r[row][col] = sum;
+        }
+    }
+}
+
+void print_matrix(double **matrix, int dim)
+{
+    for(int row = 0; row < dim; ++row)
+    {
+        printf("| ");
+        for(int col = 0; col < dim; ++col)
+        {
+            printf(" %f ", matrix[row][col]); 
+        }
+        printf(" |\n");
+    }
+   printf("\n");
 }
